850a9c5889d900d87751f1fe9a2305949ac35db9
Aleksandra1230 <ms.datsenko5@gmail.com>
2023-05-08 19:43:27 +0300

    Homework 6 redux

--- src/App.js
+++ src/App.js
@@ -1,25 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import {useDispatch, useSelector} from "react-redux";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () => {
+
+    let store = useSelector(state => state);
+    console.log(store);
+    let dispatch = useDispatch();
+
+    const increment = () => {
+        dispatch({type: 'INC', payload:2});
+
+    };
+
+    const decrement = () => {
+        dispatch({type:'DEC', payload:3});
+    };
+
+    const res = () => {
+        dispatch({type:'RESET'});
+    };
+
+    return (
+        <div>
+            <h1>{store}</h1>
+            <button onClick={increment}>INC</button>
+            <button onClick={decrement}>DEC</button>
+            <button onClick={res}>RES</button>
+        </div>
+    );
+};
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,17 +1,31 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+import {Provider} from "react-redux";
+import {createStore} from "redux";
+
+
+const baseReducer = (state = 0, action) => {
+  switch (action.type) {
+      case "INC":
+          return state +=action.payload;
+      case "DEC":
+          return state -+ action.payload;
+      case "RESET":
+          return 0;
+      default:
+          return state;
+
+  }
+}
+
+let store = createStore(baseReducer);
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <Provider store={store}>
+    <App/>
+  </Provider>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
